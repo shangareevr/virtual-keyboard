ae6c6f7e3725c724ad0e3f797974c69a48a23fe5
sh-rusel <sh.rusell@gmail.com>
2023-05-01 20:42:38 +0500

    feat:add press Caps and switch lang virtual

--- script.js
+++ script.js
@@ -45,10 +45,10 @@ class GenerateKey {
         }
       }
     });
-
     keyboard.append(key);
   }
 }
+
 function generateKeyboard() {
   keyBtns.forEach((btn) => {
     const res = new GenerateKey(btn.name, btn.rus, btn.eng, btn.rusCaps, btn.engCaps);
@@ -61,7 +61,6 @@ function animatedForPressKey() {
   window.addEventListener('keydown', (e) => {
     inputText.focus();
     const keys = document.querySelectorAll('.key');
-    // console.log(e);
     keys.forEach((key) => {
       if (key.classList.contains(e.code)) {
         key.classList.add('active');
@@ -94,16 +93,16 @@ function defineLang() {
 function changeInputLang() {
   const keysToLang = new Set();
   document.addEventListener('keydown', (e) => {
-    if (e.key === 'Control' || e.key === 'Alt') {
-      keysToLang.add(e.key);
+    if (e.code === 'ControlLeft' || e.code === 'AltLeft' || e.code === 'ControlRight' || e.code === 'AltRight') {
+      keysToLang.add(e.code);
     }
-    if (keysToLang.has('Control') && keysToLang.has('Alt')) {
+    if ((keysToLang.has('ControlLeft') && keysToLang.has('AltLeft')) || (keysToLang.has('ControlRight') && keysToLang.has('AltRight'))) {
       keysToLang.clear();
       defineLang();
     }
   });
   document.addEventListener('keyup', (e) => {
-    keysToLang.delete(e.key);
+    keysToLang.delete(e.code);
   });
 }
 changeInputLang();
@@ -114,6 +113,11 @@ function shiftKey() {
       lang = `${lang}Caps`;
       keyboard.innerHTML = '';
       generateKeyboard();
+      keyboard.childNodes.forEach((keys) => {
+        if (keys.classList.contains(`${e.code}`)) {
+          keys.classList.toggle('active');
+        }
+      });
     }
   });
   document.addEventListener('keyup', (e) => {
@@ -130,6 +134,9 @@ function capsKay() {
   document.addEventListener('keydown', (e) => {
     if (e.key === 'CapsLock') {
       keyboard.childNodes.forEach((keys) => {
+        if (keys.classList.contains('CapsLock')) {
+          keys.classList.toggle('active');
+        }
         if (keys.classList.contains('letter') || (keys.classList.contains('Backquote') && (lang === 'rus'))) {
           keys.childNodes.forEach((node) => {
             if (node.nodeName !== '#text') {
@@ -147,3 +154,59 @@ function capsKay() {
   });
 }
 capsKay();
+
+document.addEventListener('keydown', (e) => {
+  if (e.code === 'Tab') {
+    e.preventDefault();
+    inputText.value += '\t';
+  }
+});
+keyboard.addEventListener('click', (e) => {
+  if (e.target.closest('.CapsLock')) {
+    e.target.parentElement.classList.toggle('active');
+    keyboard.childNodes.forEach((keys) => {
+      if (keys.classList.contains('letter') || (keys.classList.contains('Backquote') && (lang === 'rus'))) {
+        keys.childNodes.forEach((node) => {
+          if (node.nodeName !== '#text') {
+            if (node.classList.contains(`${lang}Caps`)) {
+              node.classList.toggle('hidden-key');
+            }
+            if (node.classList.contains(`${lang}`)) {
+              node.classList.toggle('hidden-key');
+            }
+          }
+        });
+      }
+    });
+  }
+  if (e.target.closest('.ShiftLeft') || e.target.closest('.ShiftRight')) {
+    e.target.parentElement.classList.toggle('active');
+    keyboard.childNodes.forEach((keys) => {
+      keys.childNodes.forEach((node) => {
+        if (node.nodeName !== '#text') {
+          if (node.classList.contains(`${lang}Caps`)) {
+            node.classList.toggle('hidden-key');
+          }
+          if (node.classList.contains(`${lang}`)) {
+            node.classList.toggle('hidden-key');
+          }
+        }
+      });
+    });
+  }
+});
+
+function switchLangVirtial() {
+  const btnToLang = new Set();
+  keyboard.addEventListener('click', (e) => {
+    if (e.target.closest('.ControlLeft') || e.target.closest('.AltLeft') || e.target.closest('.ControlRight') || e.target.closest('.AltRight')) {
+      e.target.parentElement.classList.add('active');
+      btnToLang.add(e.target.parentElement.classList[1]);
+    }
+    if ((btnToLang.has('ControlLeft') && btnToLang.has('AltLeft')) || (btnToLang.has('ControlRight') && btnToLang.has('AltRight'))) {
+      btnToLang.clear();
+      defineLang();
+    }
+  });
+}
+switchLangVirtial();
